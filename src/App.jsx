3bd527452e1cc03e32eeed20512cb206c3f27909
kanwar Dhillon <kdhillon469@gmail.com>
2024-01-11 03:49:03 +1100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className=" text-3xl p-2 bg-purple-200 text-center opacity-2">
+        Currenct converter with chai!
+      </h1>
+    </>
+  );
+}
+
+export default App;
